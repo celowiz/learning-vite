f0ea7990ab8f67148ed12e1879a97183cece63ad
Marcelo Wizenberg <marcelo_wz@hotmail.com>
2025-08-17 12:35:58 -0300

    useState + update state

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import {useState} from "react";
 import TodoItem from "./components/TodoItem"
 import { dummyData } from "./data/todos";
 
 function App() {
+
+  const [todos, setTodos] = useState(dummyData)
+
+  function setTodoCompleted(id: number, completed: boolean) {
+    setTodos((prevTodos) => 
+      prevTodos.map(todo => (todo.id === id ? {...todo, completed} : todo))
+    );
+  }
+
   return (
     <main className="py-10 h-screen space-y-5">
       <h1 className="font-bold text-3xl text-center">
@@ -9,8 +19,12 @@ function App() {
       </h1>
       <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5">
         <div className='space-y-2'>
-          {dummyData.map(todo => (
-            <TodoItem todo={todo}/>
+          {todos.map(todo => (
+            <TodoItem 
+            key={todo.id}
+            todo={todo}
+            onCompletedChange={setTodoCompleted}
+            />
           ))}
         </div>
       </div>
