20a16df9618d20c26f6e3be67c6883f55a1c1ce6
Marcelo Wizenberg <marcelo_wz@hotmail.com>
2025-08-17 16:07:04 -0300

    sort todos; onDelete and onSubmit implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import {useState} from "react";
-import TodoItem from "./components/TodoItem"
+
 import { dummyData } from "./data/todos";
+import AddTodoForm from "./components/AddTodoForm";
+import TodoList from "./components/TodoList";
 
 function App() {
 
@@ -12,21 +14,31 @@ function App() {
     );
   }
 
+  function addTodo(title: string) {
+    setTodos(prevTodos => [
+      {
+        id: prevTodos.length + 1,
+        title,
+        completed: false,
+      },
+      ...prevTodos,
+    ])
+  }
+
+  function deleteTodo(id: number) {
+    setTodos(prevTodos => prevTodos.filter(todo => todo.id !== id))
+  }
+
   return (
-    <main className="py-10 h-screen space-y-5">
-      <h1 className="font-bold text-3xl text-center">
-        Your Todos
-      </h1>
-      <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5">
-        <div className='space-y-2'>
-          {todos.map(todo => (
-            <TodoItem 
-            key={todo.id}
-            todo={todo}
-            onCompletedChange={setTodoCompleted}
-            />
-          ))}
-        </div>
+    <main className="py-10 h-screen space-y-5 overflow-y-auto">
+      <h1 className="font-bold text-3xl text-center">Your Todos</h1>
+      <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5 space-y-6">
+        <AddTodoForm onSubmit={addTodo} />
+        <TodoList 
+        todos={todos} 
+        onCompletedChange={setTodoCompleted}
+        onDelete={deleteTodo}
+        />
       </div>
     </main>
   )
